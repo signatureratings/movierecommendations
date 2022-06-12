3b3d00f24a7ea323c03eef5d189dab19b7645a6c
balusairam <sairam@hugosave.com>
2022-06-12 13:13:06 +0530

    added mongodb conenction

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
-
-
-
+const connectDB = require("./models/connection");
 
 //middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+//database Connection
+connectDB();
+
 //routes
+app.use("/api/movie", require("./routes/movie"));
 
 
 
 
 //listen
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
